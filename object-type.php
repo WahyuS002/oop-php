002300974e4823002d04a45225cf6033a4ea03e4
WahyuS002 <syahputrawahyu61@gmail.com>
2019-10-07 20:36:10 +0700

    #5 Object Type - Menambahkan file object-type.php

---
+++ object-type.php
@@ -0,0 +1,48 @@
+<?php
+
+    class Produk{
+
+        public $judul = "judul",
+                $penerbit = "penerbit",
+                $penulis = "penulis",
+                $harga = 0;
+
+        public function __construct($judul, $penerbit, $penulis, $harga){
+            $this->judul = $judul;
+            $this->penerbit = $penerbit;
+            $this->penulis = $penulis;
+            $this->harga = $harga;
+        }
+
+        public function getLabel(){
+            return "$this->penerbit, $this->judul";
+        }
+
+    }
+
+    class CetakInfoProduk{
+        
+        //Produk menjadi acuan bahwa yang bisa diambil adalah parameter dari produk itu sendiri
+        public function cetak(Produk $produk){
+            
+            $str = ("{$produk->judul} | {$produk->getLabel()}, (Rp. {$produk->harga})");
+            return $str;
+
+        }
+
+    }
+
+    //Menimpa property public dgn cara Nama_Objek->Property_yang_ditimpa
+    $produk1 = new Produk("Naruto", "Erlangga", "Masashi Kishimoto", 1000);
+    $produk2 = new Produk("Bleach", "", "", 0);
+
+    //Mencoba menampilkan penerbit dan judul saja dgn menggunakan method getLabel
+    echo"Anime : " . $produk1->getLabel();
+    echo "<hr>";
+    echo"Anime : " . $produk2->getLabel();
+    echo "<hr>";
+
+    $infoProduk1 = new CetakInfoProduk();
+    echo $infoProduk1->cetak($produk1);
+
+?>
