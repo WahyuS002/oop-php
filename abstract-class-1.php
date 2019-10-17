0651c96dcc1fdebb25b4f0dcdb29129535a8e83a
WahyuS002 <syahputrawahyu61@gmail.com>
2019-10-17 20:49:24 +0700

    #13 Abstract Class (Bagian 1) - Catatan tentang abstact class

---
+++ abstract-class-1.php
@@ -0,0 +1,8 @@
+<?php
+
+//Abstrak adalah sebuah kerangka dasar utk class turunan
+//Abstrak tidak dapat diinstansiasi
+//Memiliki minimal 1 method abstrak example : abstract public function test();
+//Method abstract tidak perlu memiliki isi cukup untuk inisialisasi nama saja
+
+?>
