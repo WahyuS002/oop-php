b8ee4bb3c86649fbfd1b418cae5df0458401bf69
WahyuS002 <syahputrawahyu61@gmail.com>
2019-10-17 20:29:30 +0700

    #12 Constant - Menambahkan file constant.php

---
+++ constant.php
@@ -0,0 +1,11 @@
+<?php
+
+    class Coba{
+        // untuk menuliskan tipe data const tidak perlu dgn $
+        // Trivia utk penulisannya diusahakan huruf besar untuk membedakan dgn variabel
+        const PHI = 3.14;
+    }
+
+    echo Coba::PHI;
+
+?>
